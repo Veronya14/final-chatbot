760dfaafb74e05906a19c6f308d0ffb4638eaf46
unknown <nagllafathy33@gmail.com>
2022-06-08 17:06:28 +0200

    release

--- main.py
+++ main.py
@@ -116,22 +116,3 @@ def process(message):
         return(Bot)
     else:
         return("I don't understand!")
-
-from flask import Flask, render_template, request
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template("index.html")
-
-@app.route("/get")
-def get_bot_reponse():
-    userText = request.args.get('msg')
-    return str(process(userText))
-@app.route("/test")
-def say_hi():
-   
-    return str("hi")
-
-if __name__ == "__main__":
-    app.run(debug=True)
